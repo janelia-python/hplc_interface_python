799bfadfa20f322fa16420b6cbdf405b0ffe78e8
Peter Polidoro <peterpolidoro@gmail.com>
2018-11-21 10:19:47 -0500

    Create data file.

--- hplc_interface/hplc_interface.py
+++ hplc_interface/hplc_interface.py
@@ -5,9 +5,9 @@ import yaml
 from threading import Timer
 import signal
 import sys
-
-# from datetime import datetime
-# import csv
+import shutil
+from datetime import datetime
+import csv
 
 from modular_client import ModularClients
 
@@ -42,6 +42,7 @@ class HplcInterface():
     _DETECTOR = 'ultraviolet_detector_interface'
     _SAMPLE_FREQUENCY = 1
     _TIMEOUT = 4.0
+    _SECONDS_PER_MINUTE = 60
 
     def __init__(self,*args,**kwargs):
         self.is_setup = False
@@ -52,8 +53,14 @@ class HplcInterface():
             kwargs.update({'debug': DEBUG})
             self.debug = DEBUG
         self._base_path = os.path.expanduser('~/Desktop')
+        self._data_base_path = os.path.join(self._base_path,'data')
         self._args = args
         self._kwargs = kwargs
+        self._data_fieldnames = [
+            'duration',
+            'state',
+            'concentration',
+        ]
 
     def _signal_handler(self,sig,frame):
         self.stop()
@@ -66,11 +73,31 @@ class HplcInterface():
         if self.debug:
             print(*args)
 
+    def _get_date_time_str(self,timestamp=None):
+        if timestamp is None:
+            d = datetime.fromtimestamp(time.time())
+        elif timestamp == 0:
+            date_time_str = 'NULL'
+            return date_time_str
+        else:
+            d = datetime.fromtimestamp(timestamp)
+        date_time_str = d.strftime('%Y-%m-%d-%H-%M-%S')
+        return date_time_str
+
+    def _get_time_from_date_time_str(self,date_time_str):
+        if date_time_str != 'NULL':
+            timestamp = time.mktime(datetime.strptime(date_time_str,'%Y-%m-%d-%H-%M-%S').timetuple())
+        else:
+            timestamp = 0
+        return timestamp
+
     def _setup(self):
         t_start = time.time()
         self._load_config_file()
         self._setup_modular_clients()
         self._configure()
+        self._has_been_injected = False
+        self._injection_time = None
         t_end = time.time()
         self.is_setup = True
         self._debug_print('Setup time =', (t_end - t_start))
@@ -85,7 +112,7 @@ class HplcInterface():
 
     def _setup_modular_clients(self):
         print()
-        print('Detecting USB devices...')
+        print('detecting USB devices...')
         self._modular_clients = ModularClients(*self._args,**self._kwargs,timeout=self._TIMEOUT)
         hc_name = 'hplc_controller'
         hc_form_factor = '3x2'
@@ -123,20 +150,21 @@ class HplcInterface():
             else:
                 raise RuntimeError(f'Gradient property {gradient_property} not set properly.')
 
-        wavelengths = self._config['detector']['wavelengths']
+        self._wavelengths = self._config['detector']['wavelengths']
         while True:
-            wavelength_count_diff = len(wavelengths) - self._WAVELENGTH_COUNT
+            wavelength_count_diff = len(self._wavelengths) - self._WAVELENGTH_COUNT
             if wavelength_count_diff > 0:
-                wavelengths.pop()
+                self._wavelengths.pop()
             elif wavelength_count_diff < 0:
-                wavelengths.append(wavelengths[-1])
+                self._wavelengths.append(wavelengths[-1])
             else:
                 break
+        self._data_fieldnames.extend(self._wavelengths)
         if self.detector_connected:
             print()
-            self.ultraviolet_detector_interface.set_wavelengths(wavelengths)
+            self.ultraviolet_detector_interface.set_wavelengths(self._wavelengths)
             wavelengths_set = self.ultraviolet_detector_interface.get_wavelengths()
-            if wavelengths == wavelengths_set:
+            if self._wavelengths == wavelengths_set:
                 print(f'detector wavelengths set to {wavelengths_set}')
             else:
                 raise RuntimeError(f'Detector wavelengths not set properly.')
@@ -145,13 +173,56 @@ class HplcInterface():
         if not self.is_setup:
             self._setup()
         self.is_running = True
+        print()
+        print('Setting up data files.')
+        date_time_str = self._get_date_time_str()
+        self._data_path = os.path.join(self._data_base_path,date_time_str)
+        os.makedirs(self._data_path)
+        config_filename = f'config-{date_time_str}.yaml'
+        config_path = os.path.join(self._data_path,config_filename)
+        print()
+        print('copying config.yaml to:')
+        print(config_path)
+        shutil.copyfile(self._config_file_path,config_path)
+        data_filename = f'data-{date_time_str}.csv'
+        data_path = os.path.join(self._data_path,data_filename)
+        print()
+        print('creating:')
+        print(data_path)
+        print()
+        self._data_file = open(data_path,'w')
+        self._data_writer = csv.DictWriter(self._data_file,fieldnames=self._data_fieldnames)
+        self._data_writer.writeheader()
+
         self._sample_timer = Timer(1.0/self._SAMPLE_FREQUENCY,self._sample)
         self._sample_timer.start()
 
     def _sample(self):
         if self.is_setup and self.is_running:
             gradient_info = self.hplc_controller.get_gradient_info()
-            print(gradient_info)
+            state = gradient_info['state']
+            if state == 'GRADIENT_NOT_STARTED':
+                print('Waiting for injection')
+            elif state == 'FINISHED':
+                self.stop()
+                return
+            else:
+                if not self._has_been_injected:
+                    self._has_been_injected = True
+                    self._injection_time = time.time()
+                if self.detector_connected:
+                    absorbances = self.ultraviolet_detector_interface.get_absorbances()
+                else:
+                    absorbances = [wavelength*2 for wavelength in self._wavelengths]
+                data = {}
+                duration = (time.time() - self._injection_time)/self._SECONDS_PER_MINUTE
+                data['duration'] = f'{duration:.3f}'
+                data['state'] = state
+                data['concentration'] = gradient_info['concentration']
+                wavelength_absorbances = zip(self._wavelengths,absorbances)
+                for wavelength,absorbance in wavelength_absorbances:
+                    data[wavelength] = '{:.2f}'.format(absorbance)
+                self._data_writer.writerow(data)
             self._sample_timer = Timer(1.0/self._SAMPLE_FREQUENCY,self._sample)
             self._sample_timer.start()
 
@@ -160,6 +231,7 @@ class HplcInterface():
         if self.is_setup:
             self._sample_timer.cancel()
             self.hplc_controller.stop()
+            self._data_file.close()
 
 def main(args=None):
     debug = False
